fcfac72df07e964f4f24f0c0eea9d46db1c37fa0
v.podriez <v.podriez@viseven.com>
2023-11-29 16:53:17 +0200

    add if_elif_else.py

---
+++ hw3/if_elif_else.py
@@ -0,0 +1,35 @@
+def get_year():
+    year = None
+    while not year:
+        try:
+            year = int(input('Enter a year from 1900 to 1_000_000:'))
+            if str(year):
+                return year
+            raise ValueError
+        except ValueError:
+            print('The year must be an integer')
+
+
+def is_leep_year(year):
+    if not year % 400:
+        return True
+    elif not year % 100:
+        return False
+    elif not year % 4:
+        return True
+    else:
+        return False
+
+
+def check_year(year):
+    if 1900 < year < 1_000_000:
+        if is_leep_year(year):
+            print(f'{year} is a leap year')
+        else:
+            print(f'{year} is not a leap year')
+    else:
+        print('The year must be between 1900 and 1_000_000')
+
+
+checked_year = get_year()
+check_year(checked_year)
